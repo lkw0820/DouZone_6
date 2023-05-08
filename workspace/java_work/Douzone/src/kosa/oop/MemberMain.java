b9be9c4ff97db9776cca73b9e9bf2559d95f3191
lkw <gewonlee99@naver.com>
2023-05-08 14:09:40 +0900

    05.08 1

---
+++ workspace/java_work/Douzone/src/kosa/oop/MemberMain.java
@@ -0,0 +1,15 @@
+package kosa.oop;
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member m = new Member();
+		m.setName("asd");
+		m.setAge(1);
+		System.out.println("이름: "+m.getName());
+		System.out.println("나이: "+m.getAge());
+
+	}
+
+}
