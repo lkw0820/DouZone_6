5971cdc85abbf77f6de2d1afce1599a607dc0f72
lkw <gewonlee99@naver.com>
2023-05-09 12:23:03 +0900

    05.09 1

---
+++ workspace/java_work/Douzone/src/kosa/bank/Account.java
@@ -0,0 +1,49 @@
+package kosa.bank;
+
+public class Account {
+	private String id;
+	private long balance;
+	
+	//생성자
+	public Account() {
+		super();
+	}
+	public Account(String id, long balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+	
+	//method
+	//입금
+	public void deposit(long amount) {
+		this.balance+=amount;
+		System.out.println("입금완료");
+	}
+	//출금
+	public boolean withdraw(long amount) {
+		if(this.balance<amount) {
+			return false;
+		}else {
+			this.balance-=amount;
+			return true;
+		}
+	}
+	
+	
+	//getter setter
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getBalance() {
+		return balance;
+	}
+	public void setBalance(long balance) {
+		this.balance = balance;
+	};
+	
+	
+}
