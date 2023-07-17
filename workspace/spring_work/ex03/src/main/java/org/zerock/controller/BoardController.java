c65caba0f4075cdab5fe67e2cf22702104275479
lkw <gewonlee99@naver.com>
2023-07-17 11:15:39 +0900

    search 2

--- workspace/spring_work/ex03/src/main/java/org/zerock/controller/BoardController.java
+++ workspace/spring_work/ex03/src/main/java/org/zerock/controller/BoardController.java
@@ -52,9 +52,11 @@ public class BoardController {
 		if(service.remove(bno)) {
 			rttr.addFlashAttribute("result","success");
 		}
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount",cri.getAmount());
-		return "redirect:/board/list";
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount",cri.getAmount());
+//		rttr.addAttribute("type",cri.getType());
+//		rttr.addAttribute("keyword",cri.getKeyword());
+		return "redirect:/board/list"+cri.getListLink();
 	}
 	@PostMapping("/modify")
 	public String modify(BoardVO board,RedirectAttributes rttr, Criteria cri) {
@@ -63,9 +65,11 @@ public class BoardController {
 		if(service.modify(board)) {
 			rttr.addFlashAttribute("result","success");
 		}
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount",cri.getAmount());
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount",cri.getAmount());
+//		rttr.addAttribute("type",cri.getType());
+//		rttr.addAttribute("keyword",cri.getKeyword()); 이거 대신 getListLink()사용
 
-		return "redirect:/board/list";
+		return "redirect:/board/list"+cri.getListLink();
 	}
 }
